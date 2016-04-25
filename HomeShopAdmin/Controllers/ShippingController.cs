98ee1ef6004a9df1efe6e7cdda003bf7adec3906
Tyler Lutz <tlutz.computers@gmail.com>
2016-04-24 22:12:37 -0400

    Completed Edit and Delete functions. Shipping Management is complete

--- HomeShopAdmin/Controllers/ShippingController.cs
+++ HomeShopAdmin/Controllers/ShippingController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using BeyondThemes.BeyondAdmin.Models;
 using System.Linq;
+using System.Net;
+using System.Data.Entity;
 
 namespace BeyondThemes.BeyondAdmin.Controllers
 {
@@ -32,6 +34,74 @@ namespace BeyondThemes.BeyondAdmin.Controllers
             }
 
             return View(shipType);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ShippingType shipType = db.ShippingTypes.Find(id);
+
+            if(shipType == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(shipType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShippingID,ShippingName,ShippingCost")]ShippingType shipType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shipType);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ShippingType shipType = db.ShippingTypes.Find(id);
+
+            if(shipType == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(shipType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ShippingType shipType = db.ShippingTypes.Find(id);
+            db.ShippingTypes.Remove(shipType);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
