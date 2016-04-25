d7d475731c156f2ba591484990b1aa78d8c07487
Michael Opper Moser <waiting4tzeentch@gmail.com>
2016-04-24 21:32:06 -0400

    pruducts get index complete

--- HomeShopAdmin/Controllers/ProductsController.cs
+++ HomeShopAdmin/Controllers/ProductsController.cs
@@ -3,15 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BeyondThemes.BeyondAdmin.Models;
+using PagedList;
 
 namespace BeyondThemes.BeyondAdmin.Controllers
 {
     public class ProductsController : Controller
     {
+        HomeStoreEntities db = new HomeStoreEntities();
+
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View();
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var products = from p in db.Products select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.ProductName.Contains(searchString) || p.ProductDesc.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.ProductName);
+                    break;
+                case "category":
+                    products = products.OrderBy(p => p.Category);
+                    break;
+                case "cat_desc":
+                    products = products.OrderByDescending(p => p.Category);
+                    break;
+                case "quantity":
+                    products = products.OrderBy(p => p.Quantity);
+                    break;
+                case "quant_desc":
+                    products = products.OrderByDescending(p => p.Quantity);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.ProductName);
+                    break;
+            }
+
+            int pageSize = 50;
+            int pageNumber = (page ?? 1);
+
+            return View(products.ToPagedList(pageNumber, pageSize));
         }
     }
 }
