1c7bc5280b5570e4dff75b2e88ac96ed4521c5cf
Tyler Lutz <tlutz.computers@gmail.com>
2016-04-26 15:27:33 -0400

    Completed Customer Orders. Still Needs Testing

--- HomeShopAdmin/Controllers/OrdersController.cs
+++ HomeShopAdmin/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeyondThemes.BeyondAdmin.Models;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,30 @@ namespace BeyondThemes.BeyondAdmin.Controllers
 {
     public class OrdersController : Controller
     {
-        // GET: Orders
+        HomeStoreEntities db = new HomeStoreEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var orders = db.CustomerOrders;
+
+            return View(orders.OrderByDescending(o => o.OrderID));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            OrderDetails detail = new OrderDetails();
+
+            CustomerOrder order = db.CustomerOrders.Find(id);
+            detail.Order = order;
+
+            var items = db.ShoppingCartItems.Where(i => i.OrderID == order.OrderID);
+
+            foreach(ShoppingCartItem item in items)
+            {
+                detail.Items.Add(item);
+            }
+
+            return View(detail);
         }
     }
 }
